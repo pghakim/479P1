c0898f41a8097bc99f47eb30609c95479401f0eb
ezorn <39031072+ezorn@users.noreply.github.com>
2021-01-29 16:33:27 -0500

    Update 479p1.cpp

--- 479p1/479p1.cpp
+++ 479p1/479p1.cpp
@@ -1,10 +1,11 @@
 // 479p1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Program made by Emma Zorn and 
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
 
