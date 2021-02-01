86f8ebe37480300cf903ec79a98f21101c516556
Patrick <pghakim@umich.edu>
2021-02-01 12:02:43 -0500

    commit testUpdate 479p1.cpp

--- 479p1/479p1.cpp
+++ 479p1/479p1.cpp
@@ -1,11 +1,13 @@
 // 479p1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// Program made by Emma Zorn and 
+// Program made by Emma Zorn and Patrick Hakim
+#include <queue>
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    priority_queue <int> que;
     cout << "Hello World!\n";
 }
 
