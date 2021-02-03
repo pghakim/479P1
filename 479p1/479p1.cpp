cdf97aaa268aad9e664a3510c91ac27bafebee2b
Patrick <pghakim@umich.edu>
2021-02-03 16:30:46 -0500

    Revert "converted to struct/2d array of struct"
    This reverts commit d8245b0ecddf139a0e80c21c7024f741384252d7.

--- 479p1/479p1.cpp
+++ 479p1/479p1.cpp
@@ -15,91 +15,20 @@ struct compare
     }
 };
 
-
-typedef struct
+int main() 
 {
-    int x, y;
-    int num;
-    int h;
-}node;
 
-
-int CalcDistance(int row1, int column1, int row2, int column2)
-{
-    int rowDistance = row2 - row1;
-    if (rowDistance < 0)
-    {
-        rowDistance *= -1;
-    }
-    int columnDistance = column2 - column1;
-    if (columnDistance < 0)
-    {
-        columnDistance *= -1;
-    }
-    return rowDistance + columnDistance;
-};
-
-
-
-int main()
-{
-    int g = 0;
-    int f; //f(n) = g(n)+h(n)
     priority_queue<int, vector<int>, compare > que;
-    
-
-
-    node puzzle[3][3];
-
-    //1st row
-    puzzle[0][0].num = 2;   
-    puzzle[0][0].x = 0;
-    puzzle[0][0].y = 0;
-
-    puzzle[1][0].num = 8;
-    puzzle[1][0].x = 1;
-    puzzle[1][0].y = 0;
-
-    puzzle[2][0].num = 3;
-    puzzle[2][0].x = 2;
-    puzzle[2][0].y = 0;
-
-    //2nd row
-    puzzle[0][1].num = 6;
-    puzzle[0][1].x = 0;
-    puzzle[0][1].y = 1;
-
-    puzzle[1][1].num = 7;
-    puzzle[1][1].x = 1;
-    puzzle[1][1].y = 1;
-
-    puzzle[2][1].num = 4;
-    puzzle[2][1].x = 2;
-    puzzle[2][1].y = 1;
-
-    //3rd row
-    puzzle[0][2].num = 1;
-    puzzle[0][2].x = 0;
-    puzzle[0][2].y = 2;
-
-    puzzle[1][2].num = 5;
-    puzzle[1][2].x = 1;
-    puzzle[1][2].y = 2;
-
-    puzzle[2][2].num = 0;
-    puzzle[2][2].x = 2;
-    puzzle[2][2].y = 2;
- 
-    int goal[3][3] = { {1,2,3}, {8,0,4}, {7,6,5,} }; // <-- zero represents - in puzzle
+    int puzzle[3][3] = {{2,8,3}, {6,7,4}, {1,5,0,}}; // <-- zero represents - in puzzle
+    int goal[3][3] = {{1,2,3}, {8,0,4}, {7,6,5,}}; // <-- zero represents - in puzzle
 
 
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            que.push(puzzle[i][j].num);
-            cout << puzzle[i][j].num;
-            cout << " ";
+            que.push(puzzle[i][j]);
+            cout <<  puzzle[i][j];
         }
         cout << endl;
     }
